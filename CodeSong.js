80d8f293e20b16f59251a857f658488da65ab968
Rowan Douglas <rowanandvera@gmail.com>
2020-05-08 10:54:27 -0500

    added if statements to change the word lines at 1 to line and then back to lines at 0

--- CodeSong.js
+++ CodeSong.js
@@ -2,11 +2,17 @@ var friends = ["jhon", "bob", "david", "jerry", "morty"];
 for(var i=0;i<friends.length;i++){
     
 
-    for (j = 99; j >= 0; j--) {
-        
-
+    for (j = 99; j > 0; j--) {
+        var word = " lines"
+        if(j === 1){
+            var word = " line"
+        }
         var lowerNumber = [j] - 1;
-        console.log(friends[i] + ": " + [j] + " lines of code on the wall " + [j] + " lines of code; " + friends[i] +  " strikes one out, clears it all out, " +
-            lowerNumber + " lines of code in the file");
+        var word2 = " lines"
+        if(lowerNumber === 1){
+            var word2 = " line"
+        }
+        console.log(friends[i] + ": " + [j] + word + " of code on the wall " + [j] + word + " of code; " + friends[i] +  " strikes one out, clears it all out, " +
+            lowerNumber +  word2 + " of code in the file");
     }
 }

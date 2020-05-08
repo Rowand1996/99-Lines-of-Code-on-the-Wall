73d3bf6fd47004c78bb2c70d62ae8763521d0264
Rowan Douglas <rowanandvera@gmail.com>
2020-05-08 09:55:11 -0500

    think i need a for each loop got the 99 lines part

---
+++ CodeSong.js
@@ -0,0 +1,15 @@
+friends = ["jhon", "bob", "david", "jerry", "morty"];
+
+
+// for(i=0;i < friends.length;i++){
+
+
+// for (i = 99; i >= 0; i--) {
+
+
+//     var lowerNumber = [i] - 1;
+//     console.log( + ": " + [i] + " lines of code on the wall " + [i] + " lines of code; John strikes one out, clears it all out, " +
+//         lowerNumber + " lines of code in the file");
+// }
+// }
+

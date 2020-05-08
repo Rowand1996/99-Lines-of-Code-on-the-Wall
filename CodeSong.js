3a0527956e3fe3645567275dae9a874e13bdb74d
Rowan Douglas <rowanandvera@gmail.com>
2020-05-08 10:38:40 -0500

    didnt need for each loop was just doing it wrong xD but got it working

--- CodeSong.js
+++ CodeSong.js
@@ -1,15 +1,12 @@
-friends = ["jhon", "bob", "david", "jerry", "morty"];
+var friends = ["jhon", "bob", "david", "jerry", "morty"];
+for(var i=0;i<friends.length;i++){
+    
 
+    for (j = 99; j >= 0; j--) {
+        
 
-// for(i=0;i < friends.length;i++){
-
-
-// for (i = 99; i >= 0; i--) {
-
-
-//     var lowerNumber = [i] - 1;
-//     console.log( + ": " + [i] + " lines of code on the wall " + [i] + " lines of code; John strikes one out, clears it all out, " +
-//         lowerNumber + " lines of code in the file");
-// }
-// }
-
+        var lowerNumber = [j] - 1;
+        console.log(friends[i] + ": " + [j] + " lines of code on the wall " + [j] + " lines of code; " + friends[i] +  " strikes one out, clears it all out, " +
+            lowerNumber + " lines of code in the file");
+    }
+}
